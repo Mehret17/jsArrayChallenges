7a7cf3d2e4570857936c710264e1f4cc8244ed50
Mehret Kulala <merutaye@gmail.com>
2018-03-09 10:41:40 -0600

    completed challenge2

--- main.js
+++ main.js
@@ -28,3 +28,26 @@ if (firstWordCount > secondWordCount) {
 else {
   challenge1.innerHTML += secondWord.join('');
 }
+
+//challenge 2
+
+var challengeTwoInputA = [1,2,3,4,5,6,7,8,9];  //[1,2,7,4,5,6,3,8,9];
+var challengeTwoInputB = [12,13,14];  //[12,17,14];
+var challengeTwoInputC = [9,2,4,7,3];  //[9,2,4,3,7];
+
+var challenge2 = document.getElementById("challenge2");
+var inputSwap = [];
+
+for (var i = 0; i < challengeTwoInputA.length; i++){
+  var numberStringArray = challengeTwoInputA[i].toString().split('');
+    console.log(numberStringArray);
+  for(var j = 0; j < numberStringArray.length; j++){
+    if (numberStringArray[j] == '3'){
+      numberStringArray[j] = '7';
+    } else if (numberStringArray[j] == '7'){
+      numberStringArray[j] = '3';
+    }
+  }
+  inputSwap.push(Number(numberStringArray.join("")))
+}
+challenge2.innerHTML += inputSwap;
